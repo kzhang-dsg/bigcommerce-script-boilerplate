cdf8ebc412bea1e3fd82fa7338a039225d82ae86
kzhang <kzhang@dunnsolutions.com>
2023-04-05 13:35:25 -0500

    update package

--- index.ts
+++ index.ts
@@ -4,23 +4,30 @@ import { product_Full } from "bigcommerce-api-client/lib/model/generated/catalog
 import { order_Resp } from "bigcommerce-api-client/lib/model/generated/orders.v2.oas2";
 
 const bigCommerceApiClient = new BigCommerceApiClient({
-    accessToken: "xxxxxx",
-    storeHash: "xxxxxx",
+    accessToken: "XXXXX",
+    storeHash: "XXXXX",
 });
 
 async function test() {
-    const results: PaginatedData<product_Full> =
-        await bigCommerceApiClient.catalog.products.getAllProducts({
-            "date_modified:max": new Date(),
-        });
+    try {
+        const results: PaginatedData<product_Full> =
+            await bigCommerceApiClient.catalog.products.getAllProducts({
+                "date_modified:max": new Date(),
+            });
 
-    console.log(results.data && results.data[0].date_created?.toLocaleString());
+        console.log(
+            results.data && results.data[0].date_created?.toLocaleString()
+        );
 
-    const results2: order_Resp = await bigCommerceApiClient.orders.getOrder(
-        200113
-    );
+        const results2: order_Resp = await bigCommerceApiClient.orders.getOrder(
+            200113
+        );
 
-    console.log(results2);
+        console.log(results2);
+    } catch (err: any) {
+        console.log(err.message, err);
+    }
+    return;
 }
 
 test();
