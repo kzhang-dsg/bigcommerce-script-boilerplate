867457017f2f81174f2f5eef628cc98823f39208
kzhang <kzhang@dunnsolutions.com>
2023-04-09 08:55:01 -0500

    Fix pagination issue

--- index.ts
+++ index.ts
@@ -1,29 +1,22 @@
 import { BigCommerceApiClient } from "bigcommerce-api-client";
 import { PaginatedData } from "bigcommerce-api-client/lib/model/common";
 import { product_Full } from "bigcommerce-api-client/lib/model/generated/catalog.v3";
-import { order_Resp } from "bigcommerce-api-client/lib/model/generated/orders.v2.oas2";
 
 const bigCommerceApiClient = new BigCommerceApiClient({
-    accessToken: "XXXXX",
-    storeHash: "XXXXX",
+    accessToken: "xxxxx",
+    storeHash: "xxxxx",
 });
 
 async function test() {
     try {
         const results: PaginatedData<product_Full> =
             await bigCommerceApiClient.catalog.products.getAllProducts({
-                "date_modified:max": new Date(),
-            });
+                include: ["images", "custom_fields", "options", "modifiers", "variants"]
+            }, 1, 1000);
 
         console.log(
-            results.data && results.data[0].date_created?.toLocaleString()
+            JSON.stringify(results.data?.length)
         );
-
-        const results2: order_Resp = await bigCommerceApiClient.orders.getOrder(
-            200113
-        );
-
-        console.log(results2);
     } catch (err: any) {
         console.log(err.message, err);
     }
